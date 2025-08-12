6af806eb79770a28404e22dfb7002c30af8c131e
SRGustavo <gusmatoscosta@gmail.com>
2025-08-12 19:50:36 -0300

    corrigindo

--- atv_serv_nd/index.js
+++ atv_serv_nd/index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
-// Rota de teste
 app.get('/', (req, res) => {
 	res.send('Servidor Node rodando com Express!');
+
 });
 
-// Inicializa o servidor
+
 app.listen(port, () => {
 	console.log(`Servidor rodando em http://localhost:${port}`);
 });
